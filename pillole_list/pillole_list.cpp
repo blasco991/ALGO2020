2f2cedc12eb9180e239141af59c47219600d1bd0
blasco991 <blasco991@gmail.com>
2020-04-02 20:25:04 +0200

    pillole_list completed 100%

--- pillole_list/pillole_list.cpp
+++ pillole_list/pillole_list.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string solve(int n, int m = 0, string r = "") {
 
     if(n==0 && m==0)
-      return r + " ";
+      return r + "\n";
 
     return (n>0 ? solve(n - 1, m + 1, r + "I") : "") + (m>0 ? solve(n, m - 1, r + "M") : "");
 
