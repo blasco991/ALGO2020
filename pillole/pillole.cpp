3db1b0246e399d379edea7a9bb356061664b4b8d
Solomon Marian <blasco991@gmail.com>
2020-04-02 14:23:37 +0000

    trying repl.it github integration

--- pillole/pillole.cpp
+++ pillole/pillole.cpp
@@ -2,35 +2,18 @@
 
 using namespace std;
 
-string eatI(int n, int p);
-
-string eatM(int n, int p);
-
-string eatI(int n, int p) {
-
-    return "I" + eatI(n - 1, p) + "I" + eatM(n - 1, p);
-}
-
-string eatM(int n, int p) {
-
-
-    return "M" + eatI(n - 1, p) + eatM(n - 1, p);
-}
-
 string solve(int n, int m = 0, string r = "") {
-    cout << n << " " << m << endl;
 
-    string eatI = (n > 0 ? solve(n - 1, m + 1, r + "I") : "I" + r);
+    if(n==0 && m==0)
+      return r + " ";
 
-    string eatM = (m > 0 ? solve(n, m - 1, r + "M") : "I" + r);
+    return (n>0 ? solve(n - 1, m + 1, r + "I") : "") + (m>0 ? solve(n, m - 1, r + "M") : "");
 
-
-    return eatI + eatM;
 }
 
 int main() {
     int x;
     cin >> x;
-    cout << solve(x - 1);
+    cout << solve(x);
     return 0;
 }
