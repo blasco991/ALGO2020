3d5e689ce0b413bd0101a4a053557faf203dc473
blasco991 <blasco991@gmail.com>
2020-04-10 23:21:55 +0200

    rank_unrank_ABstrings completed 100%

--- rank_unrank_ABstrings/rank_search.cpp
+++ rank_unrank_ABstrings/rank_search.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
-#include <cmath>       /* pow */
+#include <bitset>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
-
+const int MAX = 100;
 int length, r;
 string input;
 
 unsigned long long ABstring2rank(string s){
-    long long n = 0;
-    for(unsigned long long i=0; i < s.length(); i++)
-        n += s[i]-'A' > 0 ? (int) pow(2,length-i-1) : 0;
-
-    return n;
+    replace( s.begin(), s.end(), 'A', '0' );
+    replace( s.begin(), s.end(), 'B', '1' );
+    return  bitset<MAX>(s).to_ulong();
 }
 
 string ABstring_of_len_and_rank(int n) {
-    string s;
-    for (int i = 0; i < length ; i++, n /= 2)
-        s += n % 2 == 1 ? 'B' : 'A';
-    reverse(s.begin(), s.end());
+    string s = bitset<MAX>(r).to_string().substr(MAX-length);
+    replace( s.begin(), s.end(), '0', 'A' );
+    replace( s.begin(), s.end(), '1', 'B' );
     return s;
 }
 
 int main() {
 
     if (scanf("%d %d", &length, &r) == 2){
-        //cout << bitset<100>(r).to_string();
         cout << ABstring_of_len_and_rank(r) << endl;
     } else {
         cin >> input;
