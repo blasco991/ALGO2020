b2ddd6634e276ed2eead7240c33482c809392205
blasco991 <blasco991@gmail.com>
2020-04-15 09:40:32 +0200

    triangolo_ricorsivo completede 100%

--- triangolo/triangolo_ricorsivo.cpp
+++ triangolo/triangolo_ricorsivo.cpp
@@ -8,22 +8,18 @@ using namespace std;
 
 const int MAX = 100;
 int m[MAX][MAX];
+int cache[MAX][MAX];
 int N;
-vector<int> accs(MAX);
 
-void traverse(int acc, int i, int j) {
-    if (i < N - 1) {
-        traverse(acc + m[i][j], i + 1, j);
-        traverse(acc + m[i][j], i + 1, j + 1);
-    } else if (accs[j] < acc + m[i][j])
-        accs[j] = acc + m[i][j];
-}
+int traverse(int i, int j) {
+    if (cache[i][j] != -1)
+        return cache[i][j];
 
-int max(int n) {
-    accs.resize(n);
-    traverse(0, 0, 0);
+    if (i == N - 1)
+        return cache[i][j] = m[i][j];
 
-    return *max_element(accs.begin(), accs.end());
+    return cache[i][j] = m[i][j] + max(traverse(i + 1, j), traverse(i + 1, j + 1));
+
 }
 
 int main() {
@@ -31,6 +27,8 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    std::fill(*cache, *cache + MAX * MAX, -1);
+
     ifstream file("input.txt");
     if (file.is_open()) {
         string line;
@@ -45,7 +43,7 @@ int main() {
         }
         file.close();
 
-        cout << max(N) << endl;
+        cout << traverse(0, 0);
 
     }
     return 0;
