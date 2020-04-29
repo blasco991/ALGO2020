f4ab2b86e71f609658c0c865649498bbf9864258
blasco991 <blasco991@gmail.com>
2020-04-29 23:00:57 +0200

    poldo_only_total 100% refactoring

--- poldo/poldo_only_total.cpp
+++ poldo/poldo_only_total.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-int N = 10000;
-vector<int> X(N);
+int N;
+vector<int> X;
 
 int main() {
 #ifdef EVAL
@@ -16,17 +16,16 @@ int main() {
     scanf("%d", &N);
     X.resize(1);
 
-    for (int i = 0, x; i < N; i++) {
-        cin >> x;
+    for (int i = 0, j, x; i < N; i++) { ;
         bool to_insert = true;
-        for (int j = 0; j < X.size() && to_insert; j++)
+        for (j = 0, cin >> x; j < X.size() && to_insert; j++)
             if (x >= X[j]) {
                 X[j] = x;
                 to_insert = false;
             }
 
         if (to_insert)
-            X.resize(X.size() + 1, x);
+            X.push_back(x);
     }
 #ifndef EVAL
     copy(X.begin(), X.end(), ostream_iterator<int>(cout, " "));
